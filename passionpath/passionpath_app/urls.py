f1b6627b0b8f206acbefdd94d901d09468bcff15
akhmadsahrul099 <akhmadsahrul099@gmail.com>
2024-11-20 22:20:30 +0700

    bisa

--- passionpath/passionpath_app/urls.py
+++ passionpath/passionpath_app/urls.py
@@ -2,18 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),                    # Halaman utama
-    path('about/', views.about, name='about'),              # Halaman about
-    path('product/', views.product, name='product'),        # Halaman product
-    path('contact/', views.contact, name='contact'),        # Halaman contact
-    path('dashboard/', views.dashboard, name='dashboard'), 
-    path('dashboard_siswa/', views.dashboard_siswa, name='dashboard_siswa'),   # Halaman dashboard
-    path('login/', views.login, name='login'),              # Halaman login
-    path('register/', views.register, name='register'),     # Halaman register
+    path('', views.index, name='index'),                    
+    path('about/', views.about, name='about'),              
+    path('product/', views.product, name='product'),        
+    path('contact/', views.contact, name='contact'),        
+    path('dashboard_admin/', views.dashboard_admin, name='dashboard_admin'), 
+    path('login/', views.user_login, name='login'), 
+    path('dashboard_siswa/', views.dashboard_siswa, name='dashboard_siswa'),            
+    path('register/', views.register, name='register'),     
     path('accounting/', views.accounting, name='accounting'),
     path('bussines/', views.bussines, name='bussines'),
     path('design/', views.design, name='design'),
     path('development/', views.development, name='development'),
     path('language/', views.language, name='language'),
+
+    path("logout/", views.user_logout, name='logout'),
 
 ]
--- passionpath_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PassionpathAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'passionpath_app'
